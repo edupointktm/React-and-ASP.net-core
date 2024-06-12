e485671bba2314f4e5e85c4b7302ef5b255f8a56
edupointktm <edupointktm@gmail.com>
2024-06-12 08:15:47 +0545

    useEffect

--- React/hook/src/hook/UseState.jsx
+++ React/hook/src/hook/UseState.jsx
@@ -1,13 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function UseState() {
     const [count, setCount]=useState(5)
+
+    const [testcount, settestCount]=useState(10)
+  
+    useEffect(()=>{
+      console.log("show me"+ count + testcount )
+    },[testcount])
+
+  let inc = ()=>{
+    setCount(count+1)
+  }
+
+  let dec=()=>{
+    setCount(count-1)
+  }
+    
   return (
     <>
     <h1> Exmaples of useState </h1>
     <hr/>
 
-     <button onClick={()=>setCount(count+1)}>+</button> {count}  <button onClick={()=>setCount(count-1)}>-</button>
+     <button onClick={()=>inc()}>+</button> 
+         {count}  
+     <button onClick={()=>dec()}>-</button>
+
+     <br/>
+     <br/>
+     <br/>
+
+     <button onClick={()=>settestCount(testcount+5)}>+</button> 
+         {testcount}  
+     <button onClick={()=>settestCount(testcount-5)}>-</button>
+     
     </>
   )
 }
